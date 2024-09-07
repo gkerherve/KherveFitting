0a438e1dae16ac11bb21b502dd1c6996b3a96929
Gwilherm Kerherve <g.kerherve@imperial.ac.uk>
2024-09-07 14:40:46 +0100

    first attempt to add lines on the plot

--- libraries/survey.py
+++ libraries/survey.py
@@ -5,6 +5,7 @@ class PeriodicTableWindow(wx.Frame):
     def __init__(self, parent):
         super().__init__(parent, title="Periodic Table",
                          style=wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
+        self.parent_window = parent  # Store the parent window
         self.SetBackgroundColour(wx.WHITE)
         self.InitUI()
 
@@ -15,11 +16,11 @@ class PeriodicTableWindow(wx.Frame):
         main_sizer = wx.BoxSizer(wx.VERTICAL)
 
         self.info_text1 = wx.StaticText(panel, style=wx.ALIGN_CENTER | wx.ST_NO_AUTORESIZE)
-        self.info_text1.SetFont(wx.Font(8, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
+        self.info_text1.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
         main_sizer.Add(self.info_text1, 0, wx.EXPAND | wx.ALL, 0)
 
         self.info_text2 = wx.StaticText(panel, style=wx.ALIGN_CENTER | wx.ST_NO_AUTORESIZE)
-        self.info_text2.SetFont(wx.Font(8, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
+        self.info_text2.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
         main_sizer.Add(self.info_text2, 0, wx.EXPAND | wx.ALL, 0)
 
         grid = wx.GridSizer(10, 18, 2, 2)
@@ -37,8 +38,8 @@ class PeriodicTableWindow(wx.Frame):
             "", "", "", "Th", "", "U", "Np", "Pu", "Am", "Cm", "", "", "", "", "", "", "", ""
         ]
 
-        button_size = (24, 24)
-        font = wx.Font(8, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
+        button_size = (30, 30)
+        font = wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
 
         for element in elements:
             if element:
@@ -46,13 +47,14 @@ class PeriodicTableWindow(wx.Frame):
                 btn.SetFont(font)
                 btn.Bind(wx.EVT_ENTER_WINDOW, self.OnElementHover)
                 btn.Bind(wx.EVT_LEAVE_WINDOW, self.OnElementLeave)
+                btn.Bind(wx.EVT_BUTTON, self.OnElementClick)
             else:
                 btn = wx.StaticText(panel, label="")
             grid.Add(btn, 0, wx.EXPAND)
 
         main_sizer.Add(grid, 0, wx.EXPAND | wx.ALL, 5)
         panel.SetSizer(main_sizer)
-        self.SetSize(460, 280)
+        self.SetSize(560, 340)
 
     def get_element_transitions(self, element):
         allowed_orbitals = ['1s', '2s', '2p', '3s', '3p', '3d', '4s', '4p', '4d', '4f', '5s', '5p', '5d', '5f']
@@ -77,49 +79,44 @@ class PeriodicTableWindow(wx.Frame):
 
     def OnElementClick(self, event):
         element = event.GetEventObject().GetLabel()
-        element_names = {
-            'H': 'Hydrogen', 'He': 'Helium', 'Li': 'Lithium', 'Be': 'Beryllium', 'B': 'Boron',
-            'C': 'Carbon', 'N': 'Nitrogen', 'O': 'Oxygen', 'F': 'Fluorine', 'Ne': 'Neon',
-            'Na': 'Sodium', 'Mg': 'Magnesium', 'Al': 'Aluminum', 'Si': 'Silicon', 'P': 'Phosphorus',
-            'S': 'Sulfur', 'Cl': 'Chlorine', 'Ar': 'Argon', 'K': 'Potassium', 'Ca': 'Calcium',
-            'Sc': 'Scandium', 'Ti': 'Titanium', 'V': 'Vanadium', 'Cr': 'Chromium', 'Mn': 'Manganese',
-            'Fe': 'Iron', 'Co': 'Cobalt', 'Ni': 'Nickel', 'Cu': 'Copper', 'Zn': 'Zinc',
-            'Ga': 'Gallium', 'Ge': 'Germanium', 'As': 'Arsenic', 'Se': 'Selenium', 'Br': 'Bromine',
-            'Kr': 'Krypton', 'Rb': 'Rubidium', 'Sr': 'Strontium', 'Y': 'Yttrium', 'Zr': 'Zirconium',
-            'Nb': 'Niobium', 'Mo': 'Molybdenum', 'Ru': 'Ruthenium', 'Rh': 'Rhodium', 'Pd': 'Palladium',
-            'Ag': 'Silver', 'Cd': 'Cadmium', 'In': 'Indium', 'Sn': 'Tin', 'Sb': 'Antimony',
-            'Te': 'Tellurium', 'I': 'Iodine', 'Xe': 'Xenon', 'Cs': 'Cesium', 'Ba': 'Barium',
-            'La': 'Lanthanum', 'Ce': 'Cerium', 'Pr': 'Praseodymium', 'Nd': 'Neodymium', 'Pm': 'Promethium',
-            'Sm': 'Samarium', 'Eu': 'Europium', 'Gd': 'Gadolinium', 'Tb': 'Terbium', 'Dy': 'Dysprosium',
-            'Ho': 'Holmium', 'Er': 'Erbium', 'Tm': 'Thulium', 'Yb': 'Ytterbium', 'Lu': 'Lutetium',
-            'Hf': 'Hafnium', 'Ta': 'Tantalum', 'W': 'Tungsten', 'Re': 'Rhenium', 'Os': 'Osmium',
-            'Ir': 'Iridium', 'Pt': 'Platinum', 'Au': 'Gold', 'Hg': 'Mercury', 'Tl': 'Thallium',
-            'Pb': 'Lead', 'Bi': 'Bismuth', 'At': 'Astatine', 'Rn': 'Radon', 'Ra': 'Radium',
-            'Th': 'Thorium', 'U': 'Uranium', 'Np': 'Neptunium', 'Pu': 'Plutonium', 'Am': 'Americium',
-            'Cm': 'Curium'
-        }
-
         transitions = self.get_element_transitions(element)
+
         if transitions:
-            info1 = f"<b>{element_names.get(element, element)}</b>: "
-            info2 = ", ".join(f"{orbital}: {be:.1f} eV" for orbital, be in transitions)
+            xmin, xmax = self.parent_window.ax.get_xlim()
+            ymin, ymax = self.parent_window.ax.get_ylim()
 
-            # Split info2 into two lines if it's too long
-            max_line_length = 60
-            if len(info2) > max_line_length:
-                split_point = info2.rfind(", ", 0, max_line_length) + 2
-                info1 += info2[:split_point]
-                info2 = info2[split_point:]
-            else:
-                info1 += info2
-                info2 = ""
+            # Filter transitions within xmin and xmax
+            valid_transitions = [t for t in transitions if xmin <= t[1] <= xmax]
 
-            self.info_text1.SetLabelMarkup(info1)
-            self.info_text2.SetLabelMarkup(info2)
-        else:
-            self.info_text1.SetLabelMarkup(f"<b>{element_names.get(element, element)}</b>: No BE transitions found")
-            self.info_text2.SetLabelMarkup("")
-        self.Layout()
+            if valid_transitions:
+                # Get RSF values for each transition
+                rsf_values = self.get_rsf_values(element, [t[0] for t in valid_transitions])
+
+                max_rsf = max(rsf_values)
+
+                # Clear previous lines
+                for line in self.parent_window.ax.lines:
+                    if line.get_label().startswith('Element_Line'):
+                        line.remove()
+
+                # Plot new lines
+                for (orbital, be), rsf in zip(valid_transitions, rsf_values):
+                    intensity = (rsf / max_rsf) * 0.6 * (ymax - ymin)
+                    self.parent_window.ax.vlines(be, ymin, ymin + intensity, color='red', linewidth=1,
+                                                 label=f'Element_Line_{element}_{orbital}')
+
+                self.parent_window.canvas.draw_idle()
+
+    def get_rsf_values(self, element, orbitals):
+        rsf_values = []
+        with open('library.lib', 'r') as file:
+            for line in file:
+                parts = line.strip().split('\t')
+                if len(parts) >= 4 and parts[0] == element and parts[2] == 'RSF':
+                    orbital = parts[1].lower()
+                    if orbital in orbitals:
+                        rsf_values.append(float(parts[3]))
+        return rsf_values
 
     def OnElementHover(self, event):
         element = event.GetEventObject().GetLabel()
